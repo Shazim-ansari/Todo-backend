e4a7e28dcef23af57c063da70950efe2dcf182e4
Shazim Nasir <camonff7@gmail.com>
2025-05-12 21:21:48 +0500

    Updated .env file and server.js

--- Server.js
+++ Server.js
@@ -1,30 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors")
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require("cors");
 const app = express();
 const router = require("./src/router/index");
 const port = 3000;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-
-mongoose
-    // .connect("mongodb://localhost:27017/todo-app")
-    .connect("mongodb+srv://shazim:shazim123@cluster0.i4a0d.mongodb.net/todo-app")
-    .then(() => console.log("MongoDB connection successful"))
+// mongoose connection
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log("MongoDB error:", err));
-    
-app.use(router);
 
+app.use(router);
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello World I Am Running");
 });
 
-
 app.listen(port, () => {
     console.log(`Server is running on port: ${port} — Good Start, Shazim`);
 });
+
